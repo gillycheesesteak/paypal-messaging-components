9477a502c8ed873dde601a1ee11a4a795295b471
Ryan Gilbert <rygilbert@paypal.com>
2019-12-04 16:49:29 -0500

    conditional script

---
+++ scripts/snapshot-tests/should-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PARENT_COMMIT_COUNT="$(git log --format=%P HEAD -n 1 | wc -w | xargs)"
+
+# When we are operating on a merge commit, target the last commit in the PR branch
+PREVIOUS_COMMIT_TARGET="$(if [[ $PARENT_COMMIT_COUNT == "2" ]]; then echo HEAD^2; else echo HEAD; fi)"
+
+PREVIOUS_COMMIT_MESSAGE="$(git log --format=%B -n 1 $PREVIOUS_COMMIT_TARGET)"
+
+if [[ $PREVIOUS_COMMIT_MESSAGE == "[update snapshot]" ]]; then 
+    echo "Found trigger commit"
+else
+    echo "Didn't find trigger commit message, skipping snapshot update..."
+    exit 1
+fi

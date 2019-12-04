8ee5f743908af7c4fdf13580b8482199c8ec22bf
Ryan Gilbert <rygilbert@paypal.com>
2019-12-04 16:12:29 -0500

    [update snapshots]

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -6,7 +6,26 @@ PREVIOUS_COMMIT_TARGET="$(if [[ $PARENT_COMMIT_COUNT == "2" ]]; then echo HEAD^2
 
 PREVIOUS_COMMIT_MESSAGE="$(git log --format=%B -n 1 $PREVIOUS_COMMIT_TARGET)"
 
+# Set up .netrc file with GitHub credentials
+# found here: https://github.com/EndBug/add-and-commit/blob/master/entrypoint.sh
+git_setup() {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "Update Snapshots"
+}
+
 if [[ $PREVIOUS_COMMIT_MESSAGE == "[update snapshot]" ]]; then 
+    git_setup
+
     echo "127.0.0.1 localhost.paypal.com" | sudo tee -a /etc/hosts
 
     npm install
@@ -18,18 +37,15 @@ if [[ $PREVIOUS_COMMIT_MESSAGE == "[update snapshot]" ]]; then
     npm run test:func -- -u
 
     echo "Pushing updated snapshots to pull request branch"
-    # TODO: Update remote URL to main repo
-    REPO_URL=https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+    # # TODO: Update remote URL to main repo
+    # REPO_URL=https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
     
-    # Allows fetching and checking out other branches
-    git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    # # Allows fetching and checking out other branches
+    # git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 
-    git config --global user.email "you@example.com"
-    git config --global user.name "Your Name"
-
-    # Allows pushing to remote
-    git remote set-url origin ${REPO_URL}
-    echo $REPO_URL
+    # # Allows pushing to remote
+    # git remote set-url origin ${REPO_URL}
+    # echo $REPO_URL
 
     git checkout ${GITHUB_REF#refs/heads/}
 

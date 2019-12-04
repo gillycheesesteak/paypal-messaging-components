33785000df00e24de92aa56d2fa11c75346b1cdd
Ryan Gilbert <gilbertstheman+github@gmail.com>
2019-12-04 14:22:37 -0500

    log script vars

--- scripts/snapshot-tests/run-tests.sh
+++ scripts/snapshot-tests/run-tests.sh
@@ -7,6 +7,14 @@ PREVIOUS_COMMIT_TARGET="$(if [[ $PARENT_COMMIT_COUNT == "2" ]]; then echo HEAD^2
 PREVIOUS_COMMIT_MESSAGE="$(git log --format=%B -n 1 $PREVIOUS_COMMIT_TARGET)"
 
 echo $PREVIOUS_COMMIT_MESSAGE
+echo GITHUB_REPOSITORY
+echo $GITHUB_REPOSITORY
+echo GITHUB_REF
+echo $GITHUB_REF
+echo GITHUB_HEAD_REF
+echo $GITHUB_HEAD_REF
+echo GITHUB_BASE_REF
+echo $GITHUB_BASE_REF
 
 if [[ $PREVIOUS_COMMIT_MESSAGE == "[update snapshot]" ]]; then 
     npm run test:func -- -u
@@ -14,7 +22,7 @@ if [[ $PREVIOUS_COMMIT_MESSAGE == "[update snapshot]" ]]; then
     echo "Pushing updated snapshots to pull request branch"
     {
         # TODO: Update remote URL to main repo
-        REPO_URL=https://${GH_TOKEN}@github.com/gillycheesesteak/paypal-messaging-components.git
+        REPO_URL=https://${GITHUB_TOKEN}@github.com/gillycheesesteak/paypal-messaging-components.git
         
         # Allows fetching and checking out other branches
         git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*

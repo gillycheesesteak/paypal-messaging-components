57613c7702ce42964d0850b3ef3155309b3d3115
Ryan Gilbert <gilbertstheman+github@gmail.com>
2020-07-27 12:58:44 -0400

    use JSX for message render

--- server/render.js
+++
@@ -1,7 +0,0 @@
-import render from 'preact-render-to-string';
-
-import Message from './message';
-
-export default function(options, markup) {
-    return render(Message({ options, markup, locale: markup.meta.offerCountry }));
-}
